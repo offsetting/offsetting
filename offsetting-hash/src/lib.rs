941fe9a702e442a266ec9489bab11de89d068af0
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2022-04-09 11:36:19 +0200

    Improved internal stuff

--- offsetting-hash/src/lib.rs
+++ offsetting-hash/src/lib.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use owo_colors::OwoColorize;
+use colored::Colorize;
 
 use crate::algo::algorithmen;
 
--- offsetting/src/main.rs
+++ offsetting/src/main.rs
@@ -1,13 +1,7 @@
-use color_eyre::eyre::Result;
-
 use crate::cli::execute;
 
 mod cli;
 
-fn main() -> Result<()> {
-  color_eyre::install()?;
-
+fn main() {
   execute();
-
-  Ok(())
 }
